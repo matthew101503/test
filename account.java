b39e9f1a4d77ffba4b920e71f59054d8557e3853
matthew101503 <142767178+matthew101503@users.noreply.github.com>
2023-08-20 23:36:47 -0400

    Add files via upload

--- account.java
+++ account.java
@@ -2,6 +2,7 @@ package ver2;
 
 public class account {
 	// Instance variable, only available inside this class.
+	//New comment for balance.
 		private double balance;
 		private String name;
 		// Constructor
